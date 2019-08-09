04f7ba54a7f24fc2569cac7f60ca15a649406f4f
YoshihitoAso <yoshihito.asoh@gmail.com>
2019-08-10 05:21:28 +0900

    remove quorum directory

--- quorum/quorum-create-account.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -u
-set -e
-
-# 変数
-pwd=`pwd`
-
-# geth 起動
-docker run --rm --name quorum-create -v $pwd/qdata:/qdata quorum /usr/local/bin/geth account new --datadir /qdata/dd --password /qdata/pass.txt
--- quorum/quorum-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -u
-set -e
-
-# 変数
-pwd=`pwd`
-
-# geth 起動
-docker run --rm -d --name quorum -v $pwd/qdata:/qdata -p 9000:9000 -p 21000:21000 -p 21000:21000/udp -p 8545:8545 -e NODE_TYPE=validator quorum
