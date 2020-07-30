577378d8d4a421dd59770d4b0bf137a44607fb83
root <root@ip-172-31-46-244.ap-northeast-1.compute.internal>
2020-07-30 07:54:31 +0000

    mod general

--- quorum-prod-general/run.sh
+++ quorum-prod-general/run.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
+#!/bin/ash
 mkdir -p /eth/geth
 
 geth --datadir "/eth" init "/eth/genesis.json"
 
+test ! -z "${rpccorsdomain}" && CORS_OPT="--rpccorsdomain \"${rpccorsdomain}\""
 geth \
 --rpc \
 --rpcaddr "0.0.0.0" \
 --rpcport "8545" \
---rpccorsdomain "*" \
+${CORS_OPT} \
 --datadir "/eth" \
 --port "30303" \
 --rpcapi "db,eth,net,web3,istanbul,personal" \
 --networkid "1500002" \
 --nat "any" \
---gasprice 0 \
+--miner.gasprice 0 \
 --verbosity 2 \
---istanbul.blockperiod 1
+--nodiscover \
+--allow-insecure-unlock
+
