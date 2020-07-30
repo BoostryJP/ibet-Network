5d4e861305e89e06e74bac9ea2363b15a34a1423
root <root@ip-172-31-46-244.ap-northeast-1.compute.internal>
2020-07-30 04:21:19 +0000

    mod validator

--- quorum-prod/run.sh
+++ quorum-prod/run.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/bin/ash
 mkdir -p /eth/geth
 
 geth --datadir "/eth" init "/eth/genesis.json"
 
+test ! -z "${rpccorsdomain}" && CORS_OPT="--rpccorsdomain \"${rpccorsdomain}\""
 geth \
 --rpc \
 --rpcaddr "0.0.0.0" \
 --rpcport "8545" \
---rpccorsdomain "*" \
+${CORS_OPT} \
 --datadir "/eth" \
 --port "30303" \
 --rpcapi "db,eth,net,web3,istanbul,personal" \
@@ -16,5 +17,8 @@ geth \
 --nodekeyhex $nodekeyhex \
 --mine \
 --syncmode "full" \
---gasprice 0 \
---verbosity 2
+--miner.gasprice 0 \
+--verbosity 2 \
+--nodiscover \
+--allow-insecure-unlock
+
