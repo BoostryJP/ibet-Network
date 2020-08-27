857fdf21c6a596d582fbf04386443b2392887043
root <root@ip-172-31-46-244.ap-northeast-1.compute.internal>
2020-08-27 06:34:04 +0000

    mod run.sh

--- quorum-prod-general/run.sh
+++ quorum-prod-general/run.sh
@@ -3,20 +3,21 @@ mkdir -p /eth/geth
 
 geth --datadir "/eth" --nousb init "/eth/genesis.json"
 
-test ! -z "${rpccorsdomain}" && CORS_OPT="--rpccorsdomain \"${rpccorsdomain}\""
-geth \
+test ! -z "${rpccorsdomain}" && CORS_OPT="--rpccorsdomain ${rpccorsdomain}"
+GETH_CMD="geth \
 --rpc \
---rpcaddr "0.0.0.0" \
---rpcport "8545" \
+--rpcaddr 0.0.0.0 \
+--rpcport 8545 \
 ${CORS_OPT} \
---datadir "/eth" \
---port "30303" \
---rpcapi "admin,debug,miner,txpool,db,eth,net,web3,istanbul,personal" \
---networkid "1500002" \
---nat "any" \
+--datadir /eth \
+--port 30303 \
+--rpcapi admin,debug,miner,txpool,db,eth,net,web3,istanbul,personal \
+--networkid 1500002 \
+--nat any \
 --miner.gasprice 0 \
 --verbosity 2 \
 --nodiscover \
 --allow-insecure-unlock \
---nousb
+--nousb"
 
+ash -c "${GETH_CMD/\*/\\\*}"
--- quorum-prod/run.sh
+++ quorum-prod/run.sh
@@ -3,23 +3,24 @@ mkdir -p /eth/geth
 
 geth --datadir "/eth" --nousb init "/eth/genesis.json"
 
-test ! -z "${rpccorsdomain}" && CORS_OPT="--rpccorsdomain \"${rpccorsdomain}\""
-geth \
+test ! -z "${rpccorsdomain}" && CORS_OPT="--rpccorsdomain ${rpccorsdomain}"
+GETH_CMD="geth \
 --rpc \
---rpcaddr "0.0.0.0" \
---rpcport "8545" \
+--rpcaddr 0.0.0.0 \
+--rpcport 8545 \
 ${CORS_OPT} \
---datadir "/eth" \
---port "30303" \
---rpcapi "admin,debug,miner,txpool,db,eth,net,web3,istanbul,personal" \
---networkid "1500002" \
---nat "any" \
+--datadir /eth \
+--port 30303 \
+--rpcapi admin,debug,miner,txpool,db,eth,net,web3,istanbul,personal \
+--networkid 1500002 \
+--nat any \
 --nodekeyhex $nodekeyhex \
 --mine \
---syncmode "full" \
+--syncmode full \
 --miner.gasprice 0 \
 --verbosity 2 \
 --nodiscover \
 --allow-insecure-unlock \
---nousb
+--nousb"
 
+ash -c "${GETH_CMD/\*/\\\*}"
