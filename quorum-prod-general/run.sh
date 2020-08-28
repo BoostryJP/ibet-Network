fe984b49c31e5272b9204d831fa343b04638e17a
root <root@ip-172-31-46-244.ap-northeast-1.compute.internal>
2020-08-28 04:13:46 +0000

    mod run.sh

--- quorum-prod-general/run.sh
+++ quorum-prod-general/run.sh
@@ -4,6 +4,8 @@ mkdir -p /eth/geth
 geth --datadir "/eth" --nousb init "/eth/genesis.json"
 
 test ! -z "${rpccorsdomain}" && CORS_OPT="--rpccorsdomain ${rpccorsdomain}"
+test ! -z "${rpcvhosts}" && VHOST_OPT="--rpcvhosts ${rpcvhosts}"
+
 GETH_CMD="geth \
 --rpc \
 --rpcaddr 0.0.0.0 \
@@ -12,7 +14,7 @@ ${CORS_OPT} \
 --datadir /eth \
 --port 30303 \
 --rpcapi admin,debug,miner,txpool,db,eth,net,web3,istanbul,personal \
---rpcvhosts * \
+${VHOST_OPT} \
 --networkid 1500002 \
 --nat any \
 --miner.gasprice 0 \
