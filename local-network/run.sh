0e8b60c77aed70bbb417a65840ad8fef01aa1baa
YoshihitoAso <y-aso@boostry.co.jp>
2022-01-17 22:06:41 +0900

    fix: Fix problem with Quorum for localnet not shutting down gracefully

--- local-network/run.sh
+++ local-network/run.sh
@@ -29,7 +29,7 @@ ash -c "nohup ${GETH_CMD//\*/\\\*} > /dev/stdout 2>&1 &"
 
 for i in $(seq 1 300); do
   sleep 1
-  ps -ef | grep -v grep | grep "geth --rpc" > /dev/null 2>&1
+  ps -ef | grep -v grep | grep "geth --datadir" > /dev/null 2>&1
   if [ $? -eq 0 ]; then
     echo "$0: geth Running."
     break
@@ -38,10 +38,10 @@ done
 
 function trap_sigint() {
   echo "$0: geth Shutdown."
-  PID=$(ps -ef | grep "geth --rpc" | grep -v grep | awk '{print $1}')
+  PID=$(ps -ef | grep "geth --datadir" | grep -v grep | awk '{print $1}')
   kill -SIGINT ${PID}
   while :; do
-    ps -ef | grep -v grep | grep "geth --rpc" > /dev/null 2>&1
+    ps -ef | grep -v grep | grep "geth --datadir" > /dev/null 2>&1
     if [ $? -ne 0 ]; then
       break
     fi
