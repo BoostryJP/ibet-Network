3f156eec191bf5a57b0bfd5bed335750b5eea94b
YoshihitoAso <y-aso@boostry.co.jp>
2022-03-02 17:32:33 +0900

    Enable the debug option

--- local-network/run.sh
+++ local-network/run.sh
@@ -14,7 +14,7 @@ GETH_CMD="geth \
 --rpc \
 --rpcaddr 0.0.0.0 \
 --rpcport 8545 \
---rpcapi db,eth,net,web3,istanbul,personal,txpool \
+--rpcapi db,eth,net,web3,istanbul,personal,txpool,debug \
 --rpccorsdomain ${rpccorsdomain} \
 --rpcvhosts ${rpcvhosts} \
 --allow-insecure-unlock \
